459e75203e97d9f157f50c88e4a92936e697955c
DANLCARTON <ericthiberge@orange.fr>
2023-10-13 18:46:56 +0200

    setup

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" >
+    <title>th6</title>
+</head>
+<body>
+    <header>
+
+        <a href="../index.html" id="accueil">Accueil</a>
+        <a href="../th6/index.html" id="suivant">Suivant</a>
+
+    </header>
+
+    <main>
+
+        <div class="cont mm">
+
+            <div class="phrases">
+
+                <i><p>Si j'étais un jeu vidéo, je serais The Last of Us II</p></i>
+                <p>Maxime Mathevet</p>
+                <a href="https://www.youtube.com/channel/UCWrmITA7UJGDTz7vOz0ZJEA"> <img src="./media/youtube.png" alt=""></a>
+                <a href="mailto:mathevet.maxime@yahoo.com"> <img src="./media/enveloppe.png" alt=""> </a>
+
+            </div>
+
+        </div>
+
+        <div class="cont et">
+
+            <div class="phrases">
+
+                <i><p>Si j'étais une typographie, je serais <span class="comic">Comic Sans MS</span></p></i>
+                <p>Eric Thiberge</p>
+                <a href="https://fr.linkedin.com/in/eric-thiberge"> <img src="./media/logo-linkedin.png" alt=""></a>
+                <a href="mailto:ericthiberge@orange.fr"> <img src="./media/enveloppe.png" alt=""> </a>
+
+            </div>
+
+        </div>
+
+    </main>
+
+</body>
+</html>
